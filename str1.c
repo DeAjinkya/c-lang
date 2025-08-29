7025aac12aba0f7f3a527f9a55b0ed7a14f422a7
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-08-29 22:56:31 +0530

    string in c lang

---
+++ str1.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+void main()
+{
+	char str[20];
+	printf("\n\tenter string: ");
+	scanf("%s", str);
+	printf("\n\n\t string is : %s", str);
+	printf("\n\n\n");
+}
+//output:-
+//enter string : hii sir
+//string is : hii
